b4856ec29558c3d0b18e26d7a45f773dd4123f74
Zach Silver <zsilver96@gmail.com>
2019-12-27 11:49:54 -0500

    added some initial ifc tests

--- tests/test_ifc.py
+++ tests/test_ifc.py
@@ -1,6 +1,37 @@
 from nerdcal.ifc import IFCDate, IFCDatetime
+import pytest
 
 
-def test_create_ifc_date():
-    ifc = IFCDate(2019, 1, 1)
+@pytest.mark.parametrize(
+    "year,month,day",
+    [
+        (2019, 1, 1),
+        (2019, 13, 1),
+        (2019, 13, 28),
+        (1, 1, 1),
+        (9999, 13, 28),
+        (2019, 13, 29),
+        (2000, 6, 29),
+    ],
+)
+def test_create_valid_ifc_date(year, month, day):
+    ifc = IFCDate(year, month, day)
     assert ifc
+
+
+@pytest.mark.parametrize(
+    "year,month,day",
+    [
+        (0, 1, 1),
+        (2019, 14, 1),
+        (2019, 0, 1),
+        (2019, 13, 30),
+        (2019, 5, 29),
+        (10000, 13, 28),
+        (2019, 13, 0),
+        (2001, 6, 29),
+    ],
+)
+def test_create_invalid_ifc_date(year, month, day):
+    with pytest.raises(ValueError):
+        IFCDate(year, month, day)
